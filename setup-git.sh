a0d78aa42f720fb6b872c335f2cf18d5885e8990
gullinbursti <biflindi.fjolnir@gmail.com>
2020-11-11 21:59:17 -0500

    using placeholder token

--- setup-git.sh
+++ setup-git.sh
@@ -23,11 +23,11 @@ git_globals() {
     printf " credential.helper as (store)…"
     git config --global credential.helper "store"
 
-    printf " credential.token as (342..54d)…"
-    git config --global credential.https://github.com.helper "342099d41c8dea76af408532dd5e4d22302b554d"
-
     printf " credential.usePath as (true)…"
-    git config --global credential.https://github.com.useHttpPath true
+    git config --global credential.http://github.com.useHttpPath true
+
+    printf " credential.token as (317..91d)…"
+    git config --global credential.http://github.com.helper "317a23d5b261a5a5557832d5f303036388be591c"
 }
 
 
