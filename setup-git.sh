93323b59cb4aa068cbe14715ffc05550b055275b
gullinbursti <code@solutionsmatty.com>
2020-11-16 12:12:21 -0500

    removing extra git stuff

--- setup-git.sh
+++ setup-git.sh
@@ -22,26 +22,21 @@ git_globals() {
 
     printf " credential.helper as (store)…"
     git config --global credential.helper "store"
+}
 
-#    printf " credential.usePath as (true)…"
-#    git config --global credential.http://github.com.usehttppath true
 
-#    printf " credential.username as (gullinbursti)…"
-#    git config -- global credential.http://github.com.username=gullinbursti
-
-#    printf " credential.token as (317..91d)…"
-#    git config --global credential.http://github.com.helper "317a23d5b261a5a5557832d5f303036388be591c"
-
-#    printf "Hard writing git creds..."
+git_creds() {
 #    echo "https://gullinbursti:317a23d5b261a5a5557832d5f303036388be591c@github.com" > ~/.git-credentials
 #    chmod 600 ~/.git-credentials
 }
 
 
 clear
-printf "Configuring for git + prepping GitHub…\n[%s=]\n\n" "`printf '=-'%.0s {1..18}`"
-
+printf "Configuring for git + prepping GitHub\n[%s=]\n\n" "`printf '=-'%.0s {1..18}`"
 git_globals "${github_username}" "${github_email}"
+
+printf "Hard writing git creds..."
+git_creds
 
 
 echo ; read -n 1 -s -r -p "Completed git setup / cfg! Press any key to quit." && echo ; echo
